bc240ad6c765f21ea60cbaffe7cd14298ef2acf6
Guilherme Santos <guipinela.santos@gmail.com>
2022-02-17 20:34:48 +0000

    commit

--- main/base/urls.py
+++ main/base/urls.py
@@ -1,9 +1,7 @@
-from django import views
 from django.urls import path, include
-from . import views
-
+from django.contrib.auth import views
+                
 urlpatterns = [
-    path('', views.loginPage, name='login'),
-    path('register/', views.registerPage, name='register'),
-    path('welcome/', views.welcomePage, name='welcome'),
+    path('acconts/', include('django.contrib.auth.urls')),
+    path('acconts/login/', views.LoginView.as_view(template_name='base/login.html')),
 ]
--- main/base/views.py
+++ main/base/views.py
@@ -5,7 +5,7 @@ from .models import Usuario
 from .forms import LoginPage, RegisterPage
 
 
-def loginPage(request):
+def LoginView(request):
     if request.method == 'POST':
         loginform == LoginPage(request.POST)
     else:
@@ -13,25 +13,3 @@ def loginPage(request):
 
     return render(request, 'base/login.html', {'loginform': loginform})
     
-
-def registerPage(request):
-    if request.method == 'POST':    
-        registerform = RegisterPage(request.POST)
-        name = request.POST['name']
-        email = request.POST['email']
-        password = request.POST['password']
-        user = Usuario(name=name, eml=email, passw=password)
-        if user is not None:
-            return HttpResponseRedirect('http://127.0.0.1:8000/')
-        else:
-            return HttpResponse('Usuário não registado!')
-    else:
-        registerform = RegisterPage()
-
-
-
-    return render(request, 'base/register.html', {'registerform': registerform})
-
-
-def welcomePage(request):
-    return render(request, 'base/welcome.html')
